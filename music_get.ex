f9597759f5fdaa33dc58718df64a805474c3168f
kokoax <yomo4808@gmail.com>
2017-01-25 21:20:18 +0900

    add

--- music_get.ex
+++ music_get.ex
@@ -1,19 +1,21 @@
 defmodule MP3Data do
   defstruct [:header, :frames, :sound_data]
-  # defstruct [:tag, :version, :flg, :size, :data]
   def new(
   <<
   tag     :: bitstring-size(24),
   version :: bitstring-size(16),
   flg     :: bitstring-size(8),
-  size    :: unsigned-integer-size(24),
-
+  size    :: unsigned-integer-size(32),
   data    :: binary,
   >>) do
+
+
+  IO.puts version
+  header               = MP3Header.getHeader(tag, version, flg, size)
+  [frames, sound_data] = MP3Frames.getFrame(version, data)
   %MP3Data{
-    # data: data,
-    header:     MP3Header.new(tag, version, flg, size),
-    frames:     MP3Frames.new(data)
+    header:     header,
+    frames:     frames,
     sound_data: sound_data,
   }
   end
@@ -21,7 +23,7 @@ end
 
 defmodule MP3Header do
   defstruct [:tag, :version, :flg, :size]
-  def new(tag, version, flg, size) do
+  def getHeader(tag, version, flg, size) do
     %MP3Header{
       tag:     tag,
       version: version,
@@ -32,34 +34,110 @@ defmodule MP3Header do
 end
 
 defmodule MP3Frames do
-  defstruct [:title, :album, :artist, :comment, :genre]
-  def new(
+  defp getFrameData(ver, size, next) when ver == << 3, 0 >>do
+    # frame_size = (size-1) * 8
+    <<
+    _ :: bitstring-size(8),    # Frame body head's 1 byte is always nil so throw away
+    data :: bitstring-size(size),
+    other :: binary,
+    >> = next
+    ret_data = getFrame(ver,other)
+    if ret_data == nil do
+      IO.inspect other
+      [data, nil, other]
+    else
+      [ret, sound_data] = ret_data
+      [data, ret, sound_data]
+    end
+  end
+
+
+  defp getFrameData(ver, size, next) when ver == << 2, 0 >>do
+    # frame_size = (size-1) * 8
+    <<
+    _ :: bitstring-size(8),    # Frame body head's 1 byte is always nil so throw away
+    data :: bitstring-size(size),
+    _ :: bitstring-size(8),    # Frame body head's 1 byte is always nil so throw away
+    other :: binary,
+    >> = next
+    ret_data = getFrame(ver,other)
+    if ret_data == nil do
+      IO.inspect other
+      [data, nil, other]
+    else
+      [ret, sound_data] = ret_data
+      [data, ret, sound_data]
+    end
+  end
+
+
+  def getFrame(ver,
   <<
-  all_data :: binary,
-  >>) do
-  %MP3Frames{
-  }
+  tag  :: bitstring-size(32),
+  size :: unsigned-integer-size(32),
+  _    :: bitstring-size(16),
+  next :: binary,
+  >>) when ver == << 3, 0 >> do
+
+  if Regex.match?(~r/^[0-9A-Z]+$/, tag) do
+    [ret, map, sound_data] = getFrameData(ver, (size-1)*8, next)
+    if map == nil do
+      [%{tag => ret}, sound_data]
+    else
+      [Map.merge(%{tag => ret}, map), sound_data]
+    end
+  end
+  end
+
+  def getFrame(
+  ver,
+  <<
+  tag  :: bitstring-size(24),
+  size :: unsigned-integer-size(24),
+  next :: binary,
+  >>) when ver == << 2, 0 >> do
+
+  if Regex.match?(~r/^[0-9A-Z]+$/, tag) do
+    [ret, map, sound_data] = getFrameData(ver, (size-2)*8, next)
+    if map == nil do
+      [%{tag => ret}, sound_data]
+    else
+      [Map.merge(%{tag => ret}, map), sound_data]
+    end
+  end
   end
 end
 
 defmodule MP3Sound do
-  defstruct [:sound_data]
-  def new(
-  size,
-  <<
-  frames     :: bitstring-size(10),
-  sound_data :: binary,
-  >>) do
-  %MP3Sound{
-    sound_data: sound_data,
-  }
+  def getSoundData(ver, size, data) when ver == << 3, 0 >> do
+    frame_size = size * 8 * 10
+    <<
+    _          :: bitstring-size(frame_size),
+    sound_data :: binary,
+    >> = data
+    sound_data
+  end
+  def getSoundData(ver, size, data) when ver == << 2, 0 >> do
+    frame_size = size * 8 * 6
+    <<
+    _          :: bitstring-size(frame_size),
+    sound_data :: binary,
+    >> = data
+    sound_data
   end
 end
 
 [path] = System.argv
 
 {:ok, file} = File.open path, [:read]
-IO.inspect MP3Data.new IO.binread(file, :all)
-# IO.inspect IO.binread(file, :all)
+
+music_data = MP3Data.new IO.binread(file, :all)
+
 File.close file
+
+{:ok, music_file} = File.open music_data.frames["TT2"], [:write]
+
+IO.binwrite music_file, music_data.sound_data
+
+File.close music_file
 
